3b370c93a46cc5be0f79205cc3224b9364e1d547
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-03-17 06:06:34 +0600

    routine design

--- ots/main/views.py
+++ ots/main/views.py
@@ -507,19 +507,19 @@ def sugesstion_class(request):
     instance = class_suggesstion.objects.filter(classes=classSugesstionChoser.classInput)
     context = {}
     context['weekday'] = 'None'
-    if today.weekday()==0:
+    if today.weekday() == 0:
         context['weekday'] = 'Monday'
-    elif today.weekday()==1:
+    elif today.weekday() == 1:
         context['weekday'] = 'Tuesday'
-    elif today.weekday()==2:
+    elif today.weekday() == 2:
         context['weekday'] = 'Wednesday'
-    elif today.weekday()==3:
+    elif today.weekday() == 3:
         context['weekday'] = 'Thursday'
-    elif today.weekday()==4:
+    elif today.weekday() == 4:
         context['weekday'] = 'Friday'
-    elif today.weekday()==5:
+    elif today.weekday() == 5:
         context['weekday'] = 'Saturday'
-    elif today.weekday()==6:
+    elif today.weekday() == 6:
         context['weekday'] = 'Sunday'
 
     context['bangla'] = 'None'
@@ -621,3 +621,133 @@ def admitCardGen(request):
                'stdClass': admitCard.stdClass, 'stdRoll': admitCard.stdRoll, 'headMaster': admitCard.headMaster}
     return render(request, 'main/admitCardGen.html', context)
 
+
+@login_required(login_url="/account/login/")
+def routine(request):
+    routine.date = request.POST.get('date')
+    routine.weekday = request.POST.get('weekday')
+    routine.date2 = request.POST.get('date2')
+    routine.weekday2 = request.POST.get('weekday2')
+    routine.date3 = request.POST.get('date3')
+    routine.weekday3 = request.POST.get('weekday3')
+    routine.date4 = request.POST.get('date4')
+    routine.weekday4 = request.POST.get('weekday4')
+    routine.date5 = request.POST.get('date5')
+    routine.weekday5 = request.POST.get('weekday5')
+    routine.date6 = request.POST.get('date6')
+    routine.weekday6 = request.POST.get('weekday6')
+    routine.date7 = request.POST.get('date7')
+    routine.weekday7 = request.POST.get('weekday7')
+    routine.p1 = request.POST.get('p1')
+    routine.p2 = request.POST.get('p2')
+    routine.p3 = request.POST.get('p3')
+    routine.p4 = request.POST.get('p4')
+    routine.p5 = request.POST.get('p5')
+    routine.p6 = request.POST.get('p6')
+    routine.p7 = request.POST.get('p7')
+    routine.n1 = request.POST.get('n1')
+    routine.n2 = request.POST.get('n2')
+    routine.n3 = request.POST.get('n3')
+    routine.n4 = request.POST.get('n4')
+    routine.n5 = request.POST.get('n5')
+    routine.n6 = request.POST.get('n6')
+    routine.n7 = request.POST.get('n7')
+    routine.c11 = request.POST.get('c11')
+    routine.c12 = request.POST.get('c12')
+    routine.c13 = request.POST.get('c13')
+    routine.c14 = request.POST.get('c14')
+    routine.c15 = request.POST.get('c15')
+    routine.c16 = request.POST.get('c16')
+    routine.c17 = request.POST.get('c17')
+    routine.c21 = request.POST.get('c21')
+    routine.c22 = request.POST.get('c22')
+    routine.c23 = request.POST.get('c23')
+    routine.c24 = request.POST.get('c24')
+    routine.c25 = request.POST.get('c25')
+    routine.c26 = request.POST.get('c26')
+    routine.c27 = request.POST.get('c27')
+    routine.c31 = request.POST.get('c31')
+    routine.c32 = request.POST.get('c32')
+    routine.c33 = request.POST.get('c33')
+    routine.c34 = request.POST.get('c34')
+    routine.c35 = request.POST.get('c35')
+    routine.c36 = request.POST.get('c36')
+    routine.c37 = request.POST.get('c37')
+    routine.c41 = request.POST.get('c41')
+    routine.c42 = request.POST.get('c42')
+    routine.c43 = request.POST.get('c43')
+    routine.c44 = request.POST.get('c44')
+    routine.c45 = request.POST.get('c45')
+    routine.c46 = request.POST.get('c46')
+    routine.c47 = request.POST.get('c47')
+    routine.c51 = request.POST.get('c51')
+    routine.c52 = request.POST.get('c52')
+    routine.c53 = request.POST.get('c53')
+    routine.c54 = request.POST.get('c54')
+    routine.c55 = request.POST.get('c55')
+    routine.c56 = request.POST.get('c56')
+    routine.c57 = request.POST.get('c57')
+    routine.c61 = request.POST.get('c61')
+    routine.c62 = request.POST.get('c62')
+    routine.c63 = request.POST.get('c63')
+    routine.c64 = request.POST.get('c64')
+    routine.c65 = request.POST.get('c65')
+    routine.c66 = request.POST.get('c66')
+    routine.c67 = request.POST.get('c67')
+    routine.c71 = request.POST.get('c71')
+    routine.c72 = request.POST.get('c72')
+    routine.c73 = request.POST.get('c73')
+    routine.c74 = request.POST.get('c74')
+    routine.c75 = request.POST.get('c75')
+    routine.c76 = request.POST.get('c76')
+    routine.c77 = request.POST.get('c77')
+    routine.c81 = request.POST.get('c81')
+    routine.c82 = request.POST.get('c82')
+    routine.c83 = request.POST.get('c83')
+    routine.c84 = request.POST.get('c84')
+    routine.c85 = request.POST.get('c85')
+    routine.c86 = request.POST.get('c86')
+    routine.c87 = request.POST.get('c87')
+    routine.c91 = request.POST.get('c91')
+    routine.c92 = request.POST.get('c92')
+    routine.c93 = request.POST.get('c93')
+    routine.c94 = request.POST.get('c94')
+    routine.c95 = request.POST.get('c95')
+    routine.c96 = request.POST.get('c96')
+    routine.c97 = request.POST.get('c97')
+    routine.c101 = request.POST.get('c101')
+    routine.c102 = request.POST.get('c102')
+    routine.c103 = request.POST.get('c103')
+    routine.c104 = request.POST.get('c104')
+    routine.c105 = request.POST.get('c105')
+    routine.c106 = request.POST.get('c106')
+    routine.c107 = request.POST.get('c107')
+    if routine.weekday and routine.date:
+        return redirect('articles:routinePrint')
+    return render(request, 'main/routine.html')
+
+
+def routinePrint(request):
+    context = {'date': routine.date, 'date2': routine.date2, 'date3': routine.date3,
+               'date4': routine.date4, 'date5': routine.date5, 'date6': routine.date6,
+               'date7': routine.date7, 'weekday': routine.weekday,
+               'weekday2': routine.weekday2, 'weekday3': routine.weekday3, 'weekday4': routine.weekday4,
+               'weekday5': routine.weekday5, 'weekday6': routine.weekday6, 'weekday7': routine.weekday7,
+               'p1': routine.p1, 'p2': routine.p2, 'p3': routine.p3, 'p4': routine.p4, 'p5': routine.p5,
+               'p6': routine.p6, 'p7': routine.p7, 'n1': routine.n1, 'n2': routine.n2, 'n3': routine.n3,
+               'n4': routine.n4, 'n5': routine.n5, 'n6': routine.n6, 'n7': routine.n7, 'c11': routine.c11,
+               'c12': routine.c12, 'c13': routine.c13, 'c14': routine.c14, 'c15': routine.c15, 'c16': routine.c16,
+               'c17': routine.c17, 'c21': routine.c21, 'c22': routine.c22, 'c23': routine.c23, 'c24': routine.c24,
+               'c25': routine.c25, 'c26': routine.c26, 'c27': routine.c27, 'c31': routine.c31, 'c32': routine.c32,
+               'c33': routine.c33, 'c34': routine.c34, 'c35': routine.c35, 'c36': routine.c36, 'c37': routine.c37,
+               'c41': routine.c41, 'c42': routine.c42, 'c43': routine.c43, 'c44': routine.c44, 'c45': routine.c45,
+               'c46': routine.c46, 'c47': routine.c47, 'c51': routine.c51, 'c52': routine.c52, 'c53': routine.c53,
+               'c54': routine.c54, 'c55': routine.c55, 'c56': routine.c56, 'c57': routine.c57, 'c61': routine.c61,
+               'c62': routine.c62, 'c63': routine.c63, 'c64': routine.c64, 'c65': routine.c65, 'c66': routine.c66,
+               'c67': routine.c67, 'c71': routine.c71, 'c72': routine.c72, 'c73': routine.c73, 'c74': routine.c74,
+               'c75': routine.c75, 'c76': routine.c76, 'c77': routine.c77, 'c81': routine.c81, 'c82': routine.c82,
+               'c83': routine.c83, 'c84': routine.c84, 'c85': routine.c85, 'c86': routine.c86, 'c87': routine.c87,
+               'c91': routine.c91, 'c92': routine.c92, 'c93': routine.c93, 'c94': routine.c94, 'c95': routine.c95,
+               'c96': routine.c96, 'c97': routine.c97, 'c101': routine.c101, 'c102': routine.c102, 'c103': routine.c103,
+               'c104': routine.c104, 'c105': routine.c105, 'c106': routine.c106, 'c107': routine.c107}
+    return render(request, 'main/routinePrint.html', context)
